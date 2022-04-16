195cd8121362df1768bac377b3b568aca79845a8
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-16 22:40:51 +0100

    added auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screens/auth_screen.dart';
 import 'package:flutter_chat_app/screens/chat_screen.dart';
 
 void main() {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         future: Firebase.initializeApp(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return const ChatScreen();
+            return const AuthScreen();
           }
           return const CircularProgressIndicator();
         },
